a886de3276b5b21e0583f1f122ff332c4ebe9c6f
Hiromu OCHIAI <otiai10@gmail.com>
2021-05-13 13:00:36 +0900

    Good bye urfave/cli

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/otiai10/opengraph/v2
 require (
 	github.com/otiai10/marmoset v0.4.0
 	github.com/otiai10/mint v1.3.2
-	github.com/urfave/cli v1.22.1
 	golang.org/x/net v0.0.0-20200923182212-328152dc79b1
 )
+
+go 1.13
--- ogp/main.go
+++ ogp/main.go
@@ -2,55 +2,52 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"net/url"
 	"os"
 
 	"github.com/otiai10/opengraph/v2"
-	"github.com/urfave/cli"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Version = "2.0.0"
-	app.Usage = "Fetch URL and extract OpenGraph meta informations."
-	app.UsageText = "ogp [-A] {URL}"
-	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:  "absolute,A",
-			Usage: "populate relative URLs to absolute URLs",
-		},
+	flagset := flag.CommandLine
+	flagset.Usage = func() {
+		fmt.Println("Fetch URL and extract OpenGraph meta informations.")
 	}
-	app.Action = func(ctx *cli.Context) error {
-		rawurl := ctx.Args().First()
-		if rawurl == "" {
-			return fmt.Errorf("URL must be specified")
-		}
-		u, err := url.Parse(rawurl)
-		if err != nil {
-			return err
-		}
-		if u.Scheme == "" {
-			u.Scheme = "https"
-		}
-		og := opengraph.New(u.String())
-		if err := og.Fetch(); err != nil {
-			return err
-		}
-		if ctx.Bool("absolute") {
-			if err := og.ToAbs(); err != nil {
-				return err
-			}
-		}
-		b, err := json.MarshalIndent(og, "", "\t")
-		if err != nil {
-			return err
-		}
-		fmt.Printf("%+v\n", string(b))
-		return nil
-	}
-	if err := app.Run(os.Args); err != nil {
+	abs := flagset.Bool("A", false, "populate relative URLs to absolute URLs")
+	flagset.Parse(os.Args[1:])
+	if err := run(flagset.Args(), *abs); err != nil {
 		fmt.Println("Error:", err.Error())
 		os.Exit(1)
 	}
+}
+
+func run(args []string, absolute bool) error {
+	if len(args) == 0 {
+		return fmt.Errorf("URL must be specified")
+	}
+	rawurl := args[0]
+	u, err := url.Parse(rawurl)
+	if err != nil {
+		return err
+	}
+	if u.Scheme == "" {
+		u.Scheme = "https"
+	}
+	og := opengraph.New(u.String())
+	if err := og.Fetch(); err != nil {
+		return err
+	}
+	if absolute {
+		if err := og.ToAbs(); err != nil {
+			return err
+		}
+	}
+	b, err := json.MarshalIndent(og, "", "\t")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%+v\n", string(b))
+	return nil
 }
